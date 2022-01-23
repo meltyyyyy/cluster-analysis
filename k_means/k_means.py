10518d3b72f1887c2c18229e898235ce5b2a5534
takeru.abe <takeru.abe@gaiax.com>
2022-01-23 09:49:41 +0900

    3-means figure

--- k_means/k_means.py
+++ k_means/k_means.py
@@ -12,3 +12,8 @@ def execute():
 
     print("Cluster membership:\n{}".format(kmeans.labels_))
 
+    fig = plt.figure()
+    mglearn.discrete_scatter(X[:, 0], X[:, 1], kmeans.labels_, markers='o')
+    mglearn.discrete_scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], [0, 1, 2], markers='^',
+                             markeredgewidth=2)
+    fig.savefig('k_means/3_means.png')
