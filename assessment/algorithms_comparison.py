9e2de32bcd14e81b9fb40ffdd26c2d6a564c36ba
takeru.abe <takeru.abe@gaiax.com>
2022-01-25 22:08:43 +0900

    kmeans assessment

--- assessment/algorithms_comparison.py
+++ assessment/algorithms_comparison.py
@@ -1,11 +1,11 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from sklearn.cluster import DBSCAN
+from sklearn.cluster import DBSCAN, KMeans
 from sklearn.datasets import fetch_lfw_people
 from sklearn.decomposition import PCA
 
 
-def dbscan(X_pca,X_people,image_shape):
+def dbscan(X_pca, X_people, image_shape):
     dbscan = DBSCAN()
     labels = dbscan.fit_predict(X_pca)
     print("Unique labels: {}".format(np.unique(labels)))
@@ -14,23 +14,35 @@ def dbscan(X_pca,X_people,image_shape):
     labels = dbscan.fit_predict(X_pca)
     print("Unique labels: {}".format(np.unique(labels)))
 
-    dbscan = DBSCAN(min_samples=3,eps=15)
+    dbscan = DBSCAN(min_samples=3, eps=15)
     labels = dbscan.fit_predict(X_pca)
     print("Unique labels: {}".format(np.unique(labels)))
     print("Number of points per cluster: {}".format(np.bincount(labels + 1)))
 
     noise = X_people[labels == -1]
-    fig,axes = plt.subplots(3,9,subplot_kw={'xticks': (),'yticks': ()},figsize=(12,4))
-    for image,ax in zip(noise,axes.ravel()):
-        ax.imshow(image.reshape(image_shape),vmin=0,vmax=1)
+    fig, axes = plt.subplots(3, 9, subplot_kw={'xticks': (), 'yticks': ()}, figsize=(12, 4))
+    for image, ax in zip(noise, axes.ravel()):
+        ax.imshow(image.reshape(image_shape), vmin=0, vmax=1)
     fig.savefig('assessment/dbscan.png')
 
-    for eps in [1,3,5,7,9,11,13]:
+    for eps in [1, 3, 5, 7, 9, 11, 13]:
         print('\neps={}'.format(eps))
-        dbscan = DBSCAN(eps=eps,min_samples=3)
+        dbscan = DBSCAN(eps=eps, min_samples=3)
         labels = dbscan.fit_predict(X_pca)
         print('Clusters present: {}'.format(np.unique(labels)))
         print('Cluster sizes: {}'.format(np.bincount(labels + 1)))
+
+
+def kmeans(X_pca, pca, image_shape):
+    km = KMeans(n_clusters=10, random_state=0)
+    labels_km = km.fit_predict(X_pca)
+    print("Cluster sizes k-means: {}".format(np.bincount(labels_km)))
+
+    fig, axes = plt.subplots(2, 5, subplot_kw={'xticks': (), 'yticks': ()}, figsize=(12, 4))
+    for center, ax in zip(km.cluster_centers_, axes.ravel()):
+        ax.imshow(pca.inverse_transform(center).reshape(image_shape), vmin=0, vmax=1)
+    fig.savefig('assessment/kmeans.png')
+
 
 def execute():
     people = fetch_lfw_people(min_faces_per_person=20, resize=0.7)
@@ -47,4 +59,5 @@ def execute():
     pca.fit(X_people)
     X_pca = pca.transform(X_people)
 
-    dbscan(X_pca,X_people,image_shape)
+    dbscan(X_pca, X_people, image_shape)
+    kmeans(X_pca, pca, image_shape)
